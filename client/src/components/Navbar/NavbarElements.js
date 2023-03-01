9bf0b4891249c2fe458c97522c2bb5550e3f689e
Matthew Bates <matthewhcbates@gmail.com>
2023-03-01 16:01:09 -0700

    working on frontend Home component

--- client/src/components/Navbar/NavbarElements.js
+++ client/src/components/Navbar/NavbarElements.js
@@ -1,36 +1,8 @@
 import styled from "styled-components";
+import CONSTANTS from "../../utils/constants";
 
 export const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 80px;
-  background: black;
-  position: sticky;
-  top: 0;
-`;
-
-export const NavbarItems = styled.div`
-  color: white;
-`;
-
-export const NavLinksWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 0.5rem;
-  margin-right: 2rem;
-  color: white;
-`;
-
-export const SidebarWrapper = styled.div`
-  top: 0;
-  left: 0;
-  position: fixed;
-  z-index: ${({ open }) => (open ? 1 : -1)};
-`;
-
-export const NavlinksWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  
+  width: 100%;
+  height: 70px;
+  background: ${CONSTANTS.colors.navy};
 `;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
-import { NavbarContainer, NavlinksWrapper } from "./NavbarElements";
-
-import Navlinks from "../Navlinks";
-import Burger from "../../components/Burger";
-import Sidebar from "../Sidebar";
+import { NavbarContainer } from "./NavbarElements";
+import { Outlet } from "react-router-dom";
 
 export default function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
     <NavbarContainer>
-      <Burger open={open} toggle={handleClick} />
-      <Sidebar setOpen={setOpen} open={open} />
-      <NavlinksWrapper>
-        <Navlinks title="home">Home</Navlinks>
-        <Navlinks title="products">Products</Navlinks>
-      </NavlinksWrapper>
+      <Outlet />
     </NavbarContainer>
   );
 }
